cd8f6f0a68055b9671acddb2d4dffc06098f5692
wuzhengzheng <408174520@qq.com>
2019-11-24 21:35:14 +0800

    FMDB添加

--- Example/Pods/Target Support Files/Pods-FFToolsKit_Example/Pods-FFToolsKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FFToolsKit_Example/Pods-FFToolsKit_Example-frameworks.sh
@@ -162,9 +162,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FFToolsKit/FFToolsKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FFToolsKit/FFToolsKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
